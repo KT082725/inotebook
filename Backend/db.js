6f7ffd63c49c62c25b57d303e0b395448e3987bf
KT082725 <aniketgupta2043@gmail.com>
2023-07-14 00:57:13 +0530

    added a remote mongo db atlas database

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const mongoURI='mongodb://127.0.0.1:27017/inotebook';
+const mongoURI='mongodb+srv://aniketgupta:Tanu0827@cluster0.kl4i2zi.mongodb.net/?retryWrites=true&w=majority';
 const connectToMongo=()=>{
     mongoose.connect(mongoURI)
 }
